716d64014129f4279a9a159a2adb5c999e399084
Ricardo Ruales <51469722+rruales6@users.noreply.github.com>
2023-12-11 12:16:37 -0500

    first commit - working
    first commit

---
+++ com-banred-application/src/main/java/com/banred/aplication/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.banred.aplication;
+
+
+import com.banred.service.MyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//EL PAQUETE A ESCANEAR PARA QUE ENCUENTRE beeans de MYSERVICE
+@SpringBootApplication(scanBasePackages = "com.banred.service")
+@RestController
+//@RequestMapping("api")
+public class DemoApplication {
+
+    private final MyService myService;
+
+    public DemoApplication(MyService myService) {
+        this.myService = myService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return myService.message();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
